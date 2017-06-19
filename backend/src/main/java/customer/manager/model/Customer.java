4f80553d7d68d6c28f4ca1c4e1cb3ea4891bbf00
evgeniya.opanasenko@outlook.com <63860DbnZ!>
2017-06-19 21:13:42 +0300

    rntity add

---
+++ backend/src/main/java/customer/manager/model/Customer.java
@@ -0,0 +1,56 @@
+package customer.manager.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String phone;
+    private String address;
+
+    public Customer() {
+    }
+
+    public Customer(String name, String phone, String address) {
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
