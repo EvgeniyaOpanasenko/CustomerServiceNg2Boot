49c374500f32ec781f058d5fcc8a8c94b9e04db7
evgeniya.opanasenko@outlook.com <63860DbnZ!>
2017-06-19 22:06:41 +0300

    database data for test s add

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO customers (name,phone,address) VALUES('Eurin', 'princess', 'Rokhan');
+INSERT INTO customers (name,phone,address) VALUES('Gendelf', 'wizard', 'some forest');
+INSERT INTO customers (name,phone,address) VALUES('Gimly', 'dworf', 'Moriya');
+INSERT INTO customers (name,phone,address) VALUES('Frodo', 'short guy', 'Shir');
